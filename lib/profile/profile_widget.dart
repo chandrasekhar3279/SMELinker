2cbe3f3932e5899b9cfd97b1749b219776dd1329
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-03 09:31:32 +0000

    Updating to latest FlutterFlow output.

--- lib/profile/profile_widget.dart
+++ lib/profile/profile_widget.dart
@@ -730,35 +730,33 @@ class _ProfileWidgetState extends State<ProfileWidget> {
                                   dense: false,
                                 ),
                               ),
-                              if (false)
-                                Divider(
-                                  thickness: 1.0,
-                                  color: FlutterFlowTheme.of(context).accent4,
-                                ),
-                              if (false)
-                                InkWell(
-                                  splashColor: Colors.transparent,
-                                  focusColor: Colors.transparent,
-                                  hoverColor: Colors.transparent,
-                                  highlightColor: Colors.transparent,
-                                  onTap: () async {
-                                    context.pushNamed('Groups');
-                                  },
-                                  child: ListTile(
-                                    title: Text(
-                                      'Groups',
-                                      style: FlutterFlowTheme.of(context)
-                                          .bodyLarge,
-                                    ),
-                                    trailing: Icon(
-                                      Icons.arrow_forward_ios,
-                                      color: FlutterFlowTheme.of(context)
-                                          .primaryText,
-                                      size: 18.0,
-                                    ),
-                                    dense: false,
+                              Divider(
+                                thickness: 1.0,
+                                color: FlutterFlowTheme.of(context).accent4,
+                              ),
+                              InkWell(
+                                splashColor: Colors.transparent,
+                                focusColor: Colors.transparent,
+                                hoverColor: Colors.transparent,
+                                highlightColor: Colors.transparent,
+                                onTap: () async {
+                                  context.pushNamed('Groups');
+                                },
+                                child: ListTile(
+                                  title: Text(
+                                    'Groups',
+                                    style:
+                                        FlutterFlowTheme.of(context).bodyLarge,
                                   ),
+                                  trailing: Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: FlutterFlowTheme.of(context)
+                                        .primaryText,
+                                    size: 18.0,
+                                  ),
+                                  dense: false,
                                 ),
+                              ),
                               Divider(
                                 thickness: 1.0,
                                 color: FlutterFlowTheme.of(context).accent4,
